2bdc1b5209c70f8c757d0664f5b4f635c4c86a77
Jose Roberto Rocha <joserobertorocha098@gmail.com>
2025-09-06 18:03:55 -0300

    Update Script.js

--- Js/Script.js
+++ Js/Script.js
@@ -294,4 +294,30 @@ document.addEventListener('DOMContentLoaded', () => {
   // init
   resize();
   start();
+
 })();
+
+/* ========projetos=======*/
+
+document.addEventListener("DOMContentLoaded", () => {
+  const modal = document.getElementById("modal");
+  const modalImg = document.getElementById("modal-img");
+  const captionText = document.getElementById("caption");
+  const closeBtn = document.querySelector(".close");
+
+  document.querySelectorAll(".expandible").forEach(img => {
+    img.addEventListener("click", () => {
+      modal.style.display = "flex";
+      modalImg.src = img.src;
+      captionText.textContent = img.alt;
+    });
+  });
+
+  closeBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+  });
+
+  modal.addEventListener("click", (e) => {
+    if (e.target === modal) modal.style.display = "none";
+  });
+});
